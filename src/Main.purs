a966f61ccf8463742171b4417b8ef0bc799d7bed
Andrea Palomba <adarkar84@gmail.com>
2020-02-24 17:39:13 +0100

    map 42

--- src/Main.purs
+++ src/Main.purs
@@ -4,12 +4,14 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 import Data.Map as Map
-
+-- import Data.Array
 import Data.Maybe (Maybe(..))
+import Data.Tuple as T
+-- import Data.Unfoldable
 import Halogen as H
 import Halogen.HTML as HH
-import Halogen.HTML.Events as HE
-import Halogen.HTML.Properties as HP
+-- import Halogen.HTML.Events as HE
+-- import Halogen.HTML.Properties as HP
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
 
@@ -19,18 +21,18 @@ data Op =
 data Config =
   Config { env :: Map.Map String Int }
 
-type State = Boolean
+type State = Config
 
 data Query a
   = Toggle a
-  | IsOn (Boolean -> a)
+  | IsOn (State -> a)
 
 type Input = Unit
 
 data Message = Toggled Boolean
 
-myButton :: forall m. H.Component HH.HTML Query Input Message m
-myButton =
+myComp :: forall m. H.Component HH.HTML Query Input Message m
+myComp =
   H.component
     { initialState: const initialState
     , render
@@ -40,26 +42,24 @@ myButton =
   where
 
   initialState :: State
-  initialState = false
+  initialState = Config { env: Map.singleton "x" 42 }
 
   render :: State -> H.ComponentHTML Query
   render state =
-    let
-      label = if state then "On" else "Off"
+    let (Config st) = state
+        s = st.env
     in
-      HH.button
-        [ HP.title label
-        , HE.onClick (HE.input_ Toggle)
-        ]
-        [ HH.text label ]
+    HH.ul_ $ map f (Map.toUnfoldable s)
+    where
+    f (T.Tuple k v) = HH.li_ [ HH.text $ k <> ": " <> show v ]
 
   eval :: Query ~> H.ComponentDSL State Query Message m
   eval = case _ of
     Toggle next -> do
       state <- H.get
-      let nextState = not state
+      let nextState = state
       H.put nextState
-      H.raise $ Toggled nextState
+      H.raise $ Toggled true
       pure next
     IsOn reply -> do
       state <- H.get
@@ -70,4 +70,4 @@ main = do
   log "Hello 🍝!"
   HA.runHalogenAff do
     body <- HA.awaitBody
-    runUI myButton unit body
+    runUI myComp unit body
